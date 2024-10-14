7bd3024000c84cbc303379a2c2c85933c03dab6f
CliftonCambel <ccroozendal@hotmail.com>
2024-10-14 22:58:48 +0200

    w

--- TTP_random_tour_and_packing_list.py
+++ TTP_random_tour_and_packing_list.py
@@ -15,7 +15,7 @@ def objective_function(tour,packinglist, items, distances, vmax, vmin, W, R): #c
     total_value = 0
     W_c = 0 #This keep track of the the total weight what is in the knapsack
     travel_time = 0
-    total_distance = 0
+    #total_distance = 0
     for i in range(len(tour)):
         current_city = tour[i]
         next_city = tour[(i + 1) % len(tour)]
@@ -51,15 +51,15 @@ def random_tour_and_packing(ttp):
         total_weight_ttp_instance += item['weight']
         if item['value']>max_value_item:
             max_value_item = item['value']
-    distance_matrix = np.array(distances)
-    min_distance = np.min(distance_matrix[distance_matrix > 0])
+   # distance_matrix = np.array(distances)
+   # min_distance = np.min(distance_matrix[distance_matrix > 0])
     vmax = 1.0
     vmin = 0.1
     Tr=0.5 #Tightness ratio interval [0,1], suggested 0.25,0.5,0.75
     W = Tr*total_weight_ttp_instance                #num_items*50/2 #might change.
-    r=0.05 # r is a random number in the interval [0.05,0.25], this will be fixed in this experiment to 0.15, to keep it more consistent and keeping it in the middle, for abitritary reasons
-    E_p=Tr*num_items*max_value_item
-    E_t = min_distance*num_cities/vmax #vague formula, don't understand why divided by vmax considering it is 1
+   # r=0.05 # r is a random number in the interval [0.05,0.25], this will be fixed in this experiment to 0.15, to keep it more consistent and keeping it in the middle, for abitritary reasons
+   # E_p=Tr*num_items*max_value_item
+   # E_t = min_distance*num_cities/vmax #vague formula, don't understand why divided by vmax considering it is 1
     R = 1.0#r*(E_p/E_t)#1.0 #might change
 
     random_tour = list(range(1, num_cities))  # Exclude starting city (0) for shuffling
@@ -93,15 +93,16 @@ def process_ttp_instances_results(input_folder, output_file,iteration):
     save_to_json(results, output_file)
 
 os.makedirs('tour_results', exist_ok=True)
-
+os.makedirs('tour_results/random_results', exist_ok=True)
+iteration = 1
 for cities in range(20, 120, 20):
     for n in range (1,5,1):     
         items=n*cities          
-        input_folder = f'json_files_TTP_instances_{cities}_items_{items}'
+        input_folder = f'problem_instances_ttp/json_files_TTP_instances_{cities}_items_{items}'
 
 # Process the TTP instances and save results
-        output_file = f'tour_results/results_random_cities_{cities}_items{items}.json'
-        process_ttp_instances_results(input_folder, output_file,1)
+        output_file = f'tour_results/random_results/results_random_iteration{iteration}_cities_{cities}_items{items}.json'
+        process_ttp_instances_results(input_folder, output_file,iteration)
 
         print(f"Results saved to {output_file}")
 
