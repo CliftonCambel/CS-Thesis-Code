cdebceb8268cd3e63a9c9ac1aaa5b63e955b49ff
CliftonCambel <ccroozendal@hotmail.com>
2024-11-14 21:58:45 +0100

    Update modules_dependencies.sh

--- modules_dependencies.sh
+++ modules_dependencies.sh
@@ -1,4 +1,5 @@
 echo 'Starting installing needed libraries'
-pip3 install matplotlib
-pip3 install --upgrade numpy
+#pip3 install matplotlib
+#pip3 install --upgrade numpy
+pip3 install tqdm
 echo 'Done with installing the libraries'
