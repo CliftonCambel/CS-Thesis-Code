11115162265f5804103ddf613d5dee93d764b5c2
CliftonCambel <ccroozendal@hotmail.com>
2024-11-14 13:45:11 +0100

    Update job_script.sh

--- job_script.sh
+++ job_script.sh
@@ -3,28 +3,24 @@
 #SBATCH --output=output_%j.log
 #SBATCH --error=error_%j.log
 #SBATCH --nodes=1
-#SBATCH --ntasks=3                # Request 3 tasks
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
 #SBATCH --partition=rome
-#SBATCH --cpus-per-task=20         # 20 CPUs per task for multithreading
-#SBATCH --time=08:00:00           # Time limit
-#SBATCH --mem=16GB                # Memory limit
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=c.c.roozendal@student.vu.nl
+#SBATCH --time=01:00:00
 
-# Load Python module
-#module load Python/3.10.4-GCCcore-11.3.0
+
 
 # Navigate to the script directory
 #cd /path/to/your/scripts
 
 # Run pythonprogram1.py on one task
-srun --ntasks=1 --cpus-per-task=20 $HOME/clif/CS-Thesis-Code/python Hillclimber_hybride_approach.py > output1.log &
+srun $HOME/clif/CS-Thesis-Code/python Hillclimber_hybride_approach.py > output1.log &
 
 # Run pythonprogram2.py on another task
-srun --ntasks=1 --cpus-per-task=20 $HOME/clif/CS-Thesis-Code/python Hillclimber_TSP_swaping.py > output2.log &
+srun  $HOME/clif/CS-Thesis-Code/python Hillclimber_TSP_swaping.py > output2.log &
 
 # Run pythonprogram2.py on another task
-srun --ntasks=1 --cpus-per-task=20 $HOME/clif/CS-Thesis-Code/python Hillclimber_TSP_swaping.py > output2.log &
+srun $HOME/clif/CS-Thesis-Code/python Hillclimber_TSP_swaping.py > output2.log &
 
 # Wait for both processes to finish
 wait
