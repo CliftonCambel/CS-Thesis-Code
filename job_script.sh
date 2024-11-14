a6a22c1100ceaba010d14cd5c267f0ccf66665e7
CliftonCambel <ccroozendal@hotmail.com>
2024-11-14 14:16:57 +0100

    Update job_script.sh

--- job_script.sh
+++ job_script.sh
@@ -11,6 +11,7 @@
 
 # Navigate to the script directory
 #cd /path/to/your/scripts
+module list
 module load 2023
 module load Python/3.11.3-GCCcore-12.3.0
 python $HOME/CS-Thesis-Code/Hillclimber_hybride_approach.py 
