6b1bc337706fe3afe910d4564000b75da3332c16
CliftonCambel <ccroozendal@hotmail.com>
2024-11-15 00:30:04 +0100

    Update job_script.sh

--- job_script.sh
+++ job_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -J Initial_test_run_hillclimber  
-#SBATCH --nodes=1                        
+#SBATCH -N 1                        
 #SBATCH --ntasks=1                       
-#SBATCH --cpus-per-task=64               
-#SBATCH --partition=rome                 
-#SBATCH --time=24:00:00                  
+#SBATCH --cpus-per-task=36               
+#SBATCH -p rome                 
+#SBATCH -t 24:00:00                  
 #SBATCH --exclusive                      
 #SBATCH --mail-type=BEGIN,END            
 #SBATCH --mail-user=c.c.roozendal@student.vu.nl  
