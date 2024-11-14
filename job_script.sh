e43152e2e2479ada188c1dccedae79e423951f91
CliftonCambel <ccroozendal@hotmail.com>
2024-11-15 00:21:42 +0100

    Update job_script.sh

--- job_script.sh
+++ job_script.sh
@@ -5,21 +5,13 @@
 #SBATCH --cpus-per-task=64               
 #SBATCH --partition=rome                 
 #SBATCH --time=24:00:00                  
-#SBATCH --mem=120G                       
 #SBATCH --exclusive                      
 #SBATCH --mail-type=BEGIN,END            
 #SBATCH --mail-user=c.c.roozendal@student.vu.nl  
 
 
-module load python                    
 
-#Copy input file to scratch
-cp $HOME/big_input_file "$TMPDIR"
- 
-#Create output directory on scratch
-mkdir "$TMPDIR"/output_d
-
-srun python $HOME/CS-Thesis-Code/Hillclimber_hybride_approach.py "$TMPDIR"/big_input_file "$TMPDIR"/output_dir
+srun python3 $HOME/CS-Thesis-Code/Hillclimber_hybride_approach.py 
 
 
 
