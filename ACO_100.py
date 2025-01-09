5b4509a9bc4e586afcea07fb2d845e2a7c304d1f
CliftonCambel <ccroozendal@hotmail.com>
2025-01-09 18:55:31 +0100

    Adjustment problem instances for final run ACO

---
+++ ACO_100.py
@@ -0,0 +1,21 @@
+import ACO
+import os
+
+
+if __name__ == "__main__":
+    os.makedirs('tour_results/aco_results_100c', exist_ok=True)
+  #  input_folders_problem_instances = []
+  #  input_folders_results_random = []
+    input_folders = []
+    output_files = []
+
+    for n in range(1,5): #return 40 to 120, remove the test from the folder names.
+        for i in range(1, 5): 
+            items = n * 100
+            name_directory = f'tour_results/aco_results_100c/TTP_instances_100_items_{items}_{i}'
+            os.makedirs(name_directory, exist_ok=True)
+            input_folder = f'problem_instances_ttp_100c/json_files_TTP_instances_100_items_{items}_{i}'
+            output_file=f'{name_directory}/results_aco_100_items_{items}_{i}.json'
+            input_folders.append(input_folder)
+            output_files.append(output_file)
+    ACO.parallel_process_ttp(input_folders, output_files)
